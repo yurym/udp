fe5ef823ee235320e4e94c42c193060df05e9aaa
Yury Mankevich <byvnis@gmail.com>
2014-06-12 19:54:56 +0300

    ver.1.2
    cli;
    NotifyIcon.

--- udp/udp_frm.Designer.vb
+++ udp/udp_frm.Designer.vb
@@ -23,6 +23,7 @@ Partial Class udp_frm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(udp_frm))
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.TextBox1 = New System.Windows.Forms.TextBox()
@@ -30,6 +31,9 @@ Partial Class udp_frm
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
@@ -51,14 +55,14 @@ Partial Class udp_frm
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
         Me.TextBox1.Size = New System.Drawing.Size(532, 281)
-        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.TabIndex = 5
         '
         'Button2
         '
         Me.Button2.Location = New System.Drawing.Point(93, 12)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 3
+        Me.Button2.TabIndex = 2
         Me.Button2.Text = "Stop"
         Me.Button2.UseVisualStyleBackColor = True
         '
@@ -79,21 +83,49 @@ Partial Class udp_frm
         Me.Button4.Location = New System.Drawing.Point(388, 12)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(75, 23)
-        Me.Button4.TabIndex = 5
+        Me.Button4.TabIndex = 3
         Me.Button4.Text = "Copy all"
         Me.Button4.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label1.Location = New System.Drawing.Point(186, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(148, 12)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "CLI: udp.exe [-start] [-tray] [-nolog]"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label2.Location = New System.Drawing.Point(186, 23)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 12)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Loaded with:"
         '
         'udp_frm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(556, 334)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "udp_frm"
         Me.Text = "udp 514 port reader"
@@ -108,5 +140,8 @@ Partial Class udp_frm
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
     Friend WithEvents Button3 As System.Windows.Forms.Button
     Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 
 End Class
--- udp/udp_frm.vb
+++ udp/udp_frm.vb
@@ -17,7 +17,38 @@ Public Class udp_frm
     Dim fake As New IPEndPoint(IPAddress.Loopback, 514)
     Dim fakesend As [Byte]() = {1}
 
+    Dim nolog As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NotifyIcon1.Text = Me.Text
+        Button2.Enabled = False 'До вызова Start()
+        Dim cmdvalue As String() = Environment.GetCommandLineArgs()
+        For Each retvalue As String In cmdvalue
+            If retvalue = "-start" Then
+                Start()
+                Label2.Text = Label2.Text & " -start"
+            ElseIf retvalue = "-tray" Then
+                FormMinimized()
+                Label2.Text = Label2.Text & " -tray"
+            ElseIf retvalue = "-nolog" Then
+                nolog = True
+                Label2.Text = Label2.Text & " -nolog"
+            End If
+        Next
+        If Label2.Text = "Loaded with:" Then Label2.Text = Label2.Text & " none"
+        'После получения nolog
+        If File.Exists(strPath) And nolog = False Then
+            streamReader = New StreamReader(strPath, True)
+            TextBox1.Text = streamReader.ReadToEnd
+            streamReader.Close()
+        End If
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Start()
+    End Sub
+
+    Private Sub Start()
         client = New UdpClient(514)
         Timer1.Interval = 10
         Timer1.Start()
@@ -33,20 +64,21 @@ Public Class udp_frm
         End If
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Button2.Enabled = False
-        If File.Exists(strPath) Then
-            streamReader = New StreamReader(strPath, True)
-            TextBox1.Text = streamReader.ReadToEnd
-            streamReader.Close()
-        End If
-    End Sub
-
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Timer1.Stop()
         Button1.Enabled = True
         Button2.Enabled = False
         client.Send(fakesend, fakesend.Length, fake)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Clipboard.Clear()
+        TextBox1.SelectAll()
+        TextBox1.Copy()
     End Sub
 
     Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
@@ -62,17 +94,8 @@ Public Class udp_frm
         Else
             TextBox1.AppendText(e.Result.ToString + vbCrLf)
             WriteLog(e.Result.ToString)
+            ErrorToTray(e.Result.ToString)
         End If
-    End Sub
-
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        TextBox1.Clear()
-    End Sub
-
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        Clipboard.Clear()
-        TextBox1.SelectAll()
-        TextBox1.Copy()
     End Sub
 
     Private Sub udp_frm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
@@ -91,13 +114,49 @@ Public Class udp_frm
     End Sub
 
     Private Sub WriteLog(ByVal strComments As String)
-        OpenFile()
-        streamWriter.WriteLine(strComments)
-        CloseFile()
+        If nolog = False Then
+            OpenFile()
+            streamWriter.WriteLine(strComments)
+            CloseFile()
+        End If
     End Sub
 
     Private Sub CloseFile()
         streamWriter.Close()
         fileStream.Close()
+    End Sub
+
+    Private Sub FormMinimized()
+        'Me.Hide() 'Под XP не работает
+        Me.WindowState = FormWindowState.Minimized
+        Me.ShowInTaskbar = False
+        NotifyIcon1.Visible = True
+        NotifyIcon1.BalloonTipText = "I'm here!"
+        NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+        NotifyIcon1.BalloonTipTitle = Me.Text
+        NotifyIcon1.ShowBalloonTip(5000)
+    End Sub
+
+    Private Sub NotifyIcon1_Click(sender As Object, e As EventArgs) Handles NotifyIcon1.Click
+        'Me.Show() 'Под XP не работает
+        Me.WindowState = FormWindowState.Normal
+        Me.ShowInTaskbar = True
+        NotifyIcon1.Visible = False
+    End Sub
+
+    Private Sub udp_frm_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        If Me.WindowState = FormWindowState.Minimized Then FormMinimized()
+        If Me.WindowState = FormWindowState.Normal Then
+            NotifyIcon1.Visible = False 'Убрать значок в трее при повторном запуске приложения
+            Button4.Focus()
+        End If
+    End Sub
+
+    Private Sub ErrorToTray(ByVal message As String)
+        If Me.WindowState = FormWindowState.Minimized Then
+            NotifyIcon1.BalloonTipText = message
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Warning
+            NotifyIcon1.ShowBalloonTip(60000)
+        End If
     End Sub
 End Class
